19412aafe76a452aa7969b966d039ec19e5d4b63
erv <ervinmitra@gmail.com>
2019-06-05 12:40:43 -0700

    explanations to entry points

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-// 1
+// 1 This will define the GraphQL schema
 const typeDefs = `
     type Query {
         info: String!
     }
 `;
 
-// 2
+// 2 The actual implementation of the GraphQL schema. 
+// Notice how its structure is identical to the Query
 const resolvers = {
     Query: {
         info: () => `This is the API of a Hackernews Clone`
     }
 };
 
-// 3
+// 3 Schema and resolvers are bundled and passed into
+// The GraphQLServer. This tells the server what API
+// operations are accepted and how they should be resolved. 
 const server = new GraphQLServer({
     typeDefs,
     resolvers,
