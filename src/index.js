c174af151e24c230b1bd27d43c53399019835206
erv <ervinmitra@gmail.com>
2019-06-12 11:21:25 -0700

    added a mutation in schema definition

--- src/index.js
+++ src/index.js
@@ -22,6 +22,10 @@ const typeDefs = `
         feed: [Link!]!,
     }
 
+    type Mutation {
+        post(url: String!, description: String!): Link!
+    }
+
     type Link {
         id: ID!
         description: String!
