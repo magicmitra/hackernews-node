01b7fe268118fbec5e5130c4a1da6bb27fd248f5
erv <ervinmitra@gmail.com>
2019-06-21 15:36:54 -0700

    clarified steps for subscriber resolver

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,7 +1,17 @@
+/**
+ * Rather than returning data directly, they return an AsyncIterator which is 
+ * subsequently used by the GraphQL server to push the data to the client.
+ */
 function newLinkSubscribe(parent, args, context, info) {
     return context.prisma.$subscribe.link({ mutation_in: ['CREATED'] }).node();
 }
 
+/**
+ * Subscription resolvers are wrapped inside an object and need to be provided 
+ * as the value for a subscribe field. Another field named 'resolve' needs to 
+ * be provided that actually returns the data from the data emitted by the
+ * AsyncIterator.
+ */
 const newLink = {
     subscribe: newLinkSubscribe,
     resolve: payload => {
