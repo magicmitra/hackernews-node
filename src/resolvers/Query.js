a07702f43f58a8790f5cd005f193238832f8148f
erv <ervinmitra@gmail.com>
2019-06-21 14:45:56 -0700

    added user resolver and schema def

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,7 +1,12 @@
 function feed(parent, args, context, info)  {
-    return context.prisma.links()
+    return context.prisma.links();
+}
+
+function user(parent, args, context, info) {
+    return context.prisma.users();
 }
 
 module.exports = { 
     feed,
+    user,
 }
